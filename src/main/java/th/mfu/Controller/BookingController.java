8c43b27e66626cf3995228240b4f768750c3c067
Nuttanon Kingkaewrungrote <6531503102@lamduan.mfu.ac.th>
2023-11-24 19:40:01 +0700

    test

--- src/main/java/th/mfu/Controller/BookingController.java
+++ src/main/java/th/mfu/Controller/BookingController.java
@@ -1,24 +1,24 @@
-package th.mfu.Controller;
+// package th.mfu.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import th.mfu.Model.BookingModel;
-import th.mfu.Model.RegisModel;
-import th.mfu.Service.BookingService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.MediaType;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
+// import th.mfu.Model.BookingModel;
+// import th.mfu.Model.RegisModel;
+// import th.mfu.Service.BookingService;
 
-import javax.validation.Valid;
-import java.util.List;
+// import javax.validation.Valid;
+// import java.util.List;
 
 
-@RestController
-public class BookingController {
-    @Autowired
-    BookingService bookingService;
-    @PostMapping(value = "/booking", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<BookingModel> booking(@Valid @RequestBody BookingModel bookingModel) {
-        return bookingService.booking(bookingModel);
-    }
-}
+// @RestController
+// public class BookingController {
+//     @Autowired
+//     BookingService bookingService;
+//     @PostMapping(value = "/booking", produces = MediaType.APPLICATION_JSON_VALUE)
+//     public List<BookingModel> booking(@Valid @RequestBody BookingModel bookingModel) {
+//         return bookingService.booking(bookingModel);
+//     }
+// }
--- src/main/java/th/mfu/Repository/BookingRepository.java
+++ src/main/java/th/mfu/Repository/BookingRepository.java
@@ -1,12 +1,12 @@
-package th.mfu.Repository;
+// package th.mfu.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import th.mfu.Domain.Booking;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+// import th.mfu.Domain.Booking;
 
-import java.util.List;
+// import java.util.List;
 
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-    List<Booking> findAllByName(String name);
-}
+// @Repository
+// public interface BookingRepository extends JpaRepository<Booking, Long> {
+//     List<Booking> findAllByName(String name);
+// }
