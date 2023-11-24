2f7604bed37a82972fa0b215c1205160f4449eb9
Nuttanon Kingkaewrungrote <6531503102@lamduan.mfu.ac.th>
2023-11-24 20:09:11 +0700

    test

--- src/main/java/th/mfu/Controller/LoginController.java
+++ src/main/java/th/mfu/Controller/LoginController.java
@@ -1,7 +1,6 @@
 // package th.mfu.Controller;
 
 // import org.apache.coyote.Response;
-// import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.http.MediaType;
 // import org.springframework.stereotype.Controller;
 // import org.springframework.ui.Model;
--- src/main/java/th/mfu/Controller/ReviewController.java
+++ src/main/java/th/mfu/Controller/ReviewController.java
@@ -1,6 +1,6 @@
 // package th.mfu.Controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+
 // import org.springframework.http.MediaType;
 // import org.springframework.web.bind.annotation.*;
 // import th.mfu.Model.BookingModel;
